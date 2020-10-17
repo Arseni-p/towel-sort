95e276d9b05c0379a72a53c18984ccae4b30c668
Arseni Pantsialei <arsenipantsialei@gmail.com>
2020-10-17 23:01:12 +0300

    fix: fix empty arr

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@
 
 module.exports = function towelSort (matrix) {
   let towelSort = [];
+
+  if (!matrix) return towelSort;
   
   for (let i = 0; i < matrix.length; i++) {
     let towelArr = matrix[i];
@@ -10,7 +12,7 @@ module.exports = function towelSort (matrix) {
     if (matrix.indexOf(towelArr) % 2 !== 0) {
       towelArr = matrix[i].reverse();
     }
-    
+
     towelSort = towelSort.concat(towelArr);
   }
   return towelSort
